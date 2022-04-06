4309ae06994448ebe646d7f46a834fc2ebb0603a
1710154@cegepat.qc.ca <>
2022-04-06 08:47:29 -0400

    Bouton Pause/Play
    J'ai fais un bouton pause et play

---
+++ Assets/Scripts/guiManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class guiManager : MonoBehaviour
+{
+    public Button btnPause;
+    public Button btnResume;
+    public static bool GameIsPaused = false;
+    float currentTime;
+    public Text currentTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+        btnPause.onClick.AddListener(btnPauseClick);
+        btnResume.onClick.AddListener(btnResumeClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Bouton pause
+        if (GameIsPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        if (!GameIsPaused)
+        {
+            Time.timeScale = 1f;
+        }
+        //Chronomètre
+        currentTime = currentTime + Time.deltaTime;
+        TimeSpan time =  TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:ff");
+    }
+
+    void btnPauseClick()
+    {
+        //Pause le jeu
+        GameIsPaused = true ;
+    }
+
+    void btnResumeClick()
+    {
+        //Reprends le jeu
+        GameIsPaused = false ;
+    }
+}
