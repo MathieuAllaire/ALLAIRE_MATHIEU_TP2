ccbe925990e5cd005a65944749e9d242b55ab346
1710154@cegepat.qc.ca <>
2022-04-08 17:05:34 -0400

    Finalisation Heatlhbar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,16 +7,18 @@ public class HealthBar : MonoBehaviour
     [SerializeField]
     private Transform bar;
 
+    public Camera mainCam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(mainCam.transform);
     }
 
     public void SetSize(float sizeNormalized)
